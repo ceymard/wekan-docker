7a5cdd13178b5aa580e9d0b7af4f9f474996c036
Christophe Eymard <christophe.eymard@gmail.com>
2018-06-21 00:29:36 +0200

    ...

--- src/run.ts
+++ src/run.ts
@@ -16,18 +16,45 @@ async function run() {
 
   const dock = new Dockerode({socketPath: '/var/run/docker.sock'})
 
-  const cts = await dock.listContainers({all: true})
-  for (var _ of cts) {
+  const projects = [] as {
+    name: string
+    containers: Dockerode.ContainerInspectInfo[]
+  }[]
 
+  const cts = await dock.listContainers({all: true})
+  const pr = (a: Dockerode.ContainerInfo) => a.Labels['com.docker.compose.project']
+  cts.sort((a, b) => pr(a) < pr(b) ? -1 : pr(a) > pr(b) ? 1 : 0)
+  for (var _ of cts) {
     const c = await dock.getContainer(_.Id)
     const info = await c.inspect()
-    // info.Config.Env
-    console.log(inspect(info, {colors: true, depth: null}))
-  }
-  // console.log(cts)
+    const project = _.Labels['com.docker.compose.project']
 
-  // Get current user boards
-  // const board = (await client.getDockerBoard())
+    const last = projects[projects.length - 1]
+    if (last && last.name === project) {
+      last.containers.push(info)
+    } else {
+      projects.push({
+        name: project,
+        containers: [info]
+      })
+    }
+  }
+
+  for (var p of projects) {
+    const title = p.name
+    const urls = p.containers.map(c => {
+      for (var e of c.Config.Env) {
+        // console.log(e)
+        if (e.startsWith('VIRTUAL_HOST=')) {
+          e = e.replace('VIRTUAL_HOST=', '')
+          return e
+        }
+      }
+      return ''
+    }).filter(a => a).join(', ')
+    const running = p.containers.map(c => c.State.Running).filter(a => a).length > 0
+    console.log(title, urls, running)
+  }
 }
 
 run().catch(e => console.error(e.stack))
